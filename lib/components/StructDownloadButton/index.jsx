7090b44793b902b5665ff0d2fbb677235e8c8b79
Kristjan Eimre <kristjaneimre@gmail.com>
2024-12-06 15:03:52 +0200

    StructDownloadButton - rm chemdoodle format

--- lib/components/StructDownloadButton/index.jsx
+++ lib/components/StructDownloadButton/index.jsx
@@ -5,29 +5,30 @@ import Popover from "react-bootstrap/Popover";
 
 import styles from "./styles.module.css";
 
-export function StructDownloadButton(props) {
-  var dl_url = `${props.aiida_rest_url}/nodes/${props.uuid}/download`;
+const defaultFormats = [
+  { format: "cif", label: "CIF" },
+  { format: "xsf", label: "XSF" },
+  { format: "xyz", label: "XYZ" },
+];
 
-  var clickPopover = (
+export function StructDownloadButton(props) {
+  const dl_url = `${props.aiida_rest_url}/nodes/${props.uuid}/download`;
+  const downloadFormats = props.download_formats || defaultFormats;
+
+  const clickPopover = (
     <Popover>
       <Popover.Body style={{ padding: "5px 0px" }}>
         <ul className={styles.download_dropdown_menu}>
-          <li>
-            <a href={`${dl_url}?download_format=chemdoodle`}>ChemDoodle</a>
-          </li>
-          <li>
-            <a href={`${dl_url}?download_format=cif`}>CIF</a>
-          </li>
-          <li>
-            <a href={`${dl_url}?download_format=xsf`}>XSF</a>
-          </li>
-          <li>
-            <a href={`${dl_url}?download_format=xyz`}>XYZ</a>
-          </li>
+          {downloadFormats.map(({ format, label }) => (
+            <li key={format}>
+              <a href={`${dl_url}?download_format=${format}`}>{label}</a>
+            </li>
+          ))}
         </ul>
       </Popover.Body>
     </Popover>
   );
+
   return (
     <OverlayTrigger
       trigger="click"
